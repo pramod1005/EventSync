3e78d5dc393a33c345350724059d6a6bade144e9
pramod1005 <ksrinivaspramod@gmail.com>
2026-08-31 14:43:57 +0530

    Frontend Deplyment changes

--- src/Users/Guesthome.jsx
+++ src/Users/Guesthome.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import { useEffect, useState } from 'react';
-import './GuestHome.css';
+import './Guesthome.css';
 import API_URL from "../config";
 
 
